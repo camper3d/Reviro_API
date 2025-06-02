dcb024036877839ef0b7bc9a1c34ef2956736601
camper3d <camper3d@gmail.com>
2025-06-02 13:54:12 +0300

    pydantic схема

---
+++ schemas.py
@@ -0,0 +1,33 @@
+from pydantic import BaseModel
+from typing import Optional
+from datetime import date
+from enum import Enum
+
+
+class EnumStatus(str, Enum):
+    new = 'new'
+    in_progress = 'in_progress'
+    done = 'done'
+
+
+class TaskBase(BaseModel):
+    title: str
+    description: Optional[str] = None
+    due_date: Optional[date]
+    status: EnumStatus = EnumStatus.new
+
+
+class TaskCreate(TaskBase):
+    pass
+
+
+class TaskUpdate(TaskBase):
+    pass
+
+
+class TaskOut(TaskBase):
+    id: int
+
+
+    class Config:
+        orm_mode = True
